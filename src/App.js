bae5b1d8725e635b25fb1eb2ebdff22f24311d09
serg155alternate <serg1557733@i.ua>
2021-11-22 18:56:28 +0200

    add props and states use arrow func

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+class WhoAmI extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        years: 27,
+        text: 'tertser',
+        position: ''
+    }
+    //this.nextYear = this.nextYear.bind(this);
+  }
+  nextYear = () =>  {
+        this.setState(state =>({
+            years: state.years + 1
+        }))
+  }
+  next = () => {
+        this.setState(state =>(
+            {
+          text: state.text +'*',
+          years: state.years - 1,
+      }
+        ))
+}
+    commitInputChanges = (e, color) => {
+        console.log(color);
+       
+        this.setState ({
+        position: e.target.value
+    })
+}
+
+    render() { 
+        
+            const {name, surname, link} = this.props;
+            const {position, years, text} = this.state;
+            return (
+                <div>
+                    <button onClick = {this.nextYear}>{text}</button>
+                    <button onClick={this.next}>{text}</button>
+                    <h1>My name is {name} , surname : {surname},
+                     my age {years}, This state position:{position} </h1>
+                    <a href={link}>My Profile</a>
+                    <from>
+                        <span>Enter employer</span>
+                        <input type="text"onChange= {(e) => this.commitInputChanges(e, 'some color')} />
+                    </from>
+                </div>
+                ) 
+        }
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">
+        <WhoAmI name='Alex' surname='Borsuk' link='www.leningad.ru'/>
+        <WhoAmI name='Ivan' surname='ivanov' link='www.leningad.ru'/>
+        </h1>
+        
     </div>
   );
 }
 
 export default App;
+
+
