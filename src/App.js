b549fa283141a79960b0f9cc66430ebc0e2658d4
serg155alternate <serg1557733@ukr.net>
2022-01-06 19:26:07 +0200

    start paginations

--- src/App.js
+++ src/App.js
@@ -7,31 +7,39 @@ function App() {
 const [posts, setPosts] = useState([]);
 const [newPost, setnewPost] = useState({title: '', body: ''});
 
-useEffect(() => fetchPosts(),[])
 
 
 
-//decomposition later
-const [isLoading, setIsLoading] = useState(false)
 
-const URL_API = 'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=3';
+//decomposition will be later
 
-const  fetchPosts = async (url) => {
+const [isLoading, setIsLoading] = useState(false) // loading
+const [totalCount, setTotalCount] = useState(0)  // paginations start 0
+const [limit, setLimit] = useState(10); //posts on page
+const [page, setPage] = useState(1);//statrt from first page
+
+
+const URL_API = 'https://jsonplaceholder.typicode.com/posts/';
+
+const  fetchPosts = async (url, limit = 10, page = 1) => {
     setIsLoading(true)
     try{
-        const response = await fetch(URL_API);
+        const response = await fetch(`${url}?_limit=${limit}&_page=${page}`);
         const result = await response.json()
+        setTotalCount(response.headers.get('X-Total-Count'))
         setPosts(result);
     } catch (e){
         console.log(e)
     }
-    setIsLoading(false)
-  
+    setIsLoading(false)  
 }
+
+
+
 
 //
 
-
+useEffect(() => fetchPosts(URL_API, limit, page),[])
 
 
 const deletePost = (id) => {
@@ -49,17 +57,19 @@ const addPost = () => {
 
 return (
       <div className="App-header">
-          <button onClick={addPost}>add post</button>
-
-          
-          <input type="text" 
-                name="name" id="1" 
-                value={newPost.body}
-                onChange={e => {setnewPost({...newPost, body: e.target.value})}}/>
+          <div className="addPost">
+            
             <input type="text" 
-                name="title" id="1" 
-                value={newPost.title}
-                onChange={e => {setnewPost({...newPost,title: e.target.value})}}/>
+                    name="name" id="1" 
+                    value={newPost.body}
+                    onChange={e => {setnewPost({...newPost, body: e.target.value})}}/>
+                <input type="text" 
+                    name="title" id="1" 
+                    value={newPost.title}
+                    onChange={e => {setnewPost({...newPost,title: e.target.value})}}/>
+                <button onClick={addPost}>ADD POST</button>
+          </div>
+          
 
           {isLoading? 
           <h2 style={{textAlign:'center', color:'blue'}}>loading...</h2>
