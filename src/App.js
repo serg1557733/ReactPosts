a14eba788c631506e2d3a7b773a474bf7d9ece0b
serg155alternate <serg1557733@ukr.net>
2022-01-06 18:11:26 +0200

    md

--- src/App.js
+++ src/App.js
@@ -1,72 +1,51 @@
-import { Component } from 'react';
-import './App.css';
-
-class WhoAmI extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-        years: 27,
-        text: 'tertser',
-        position: ''
-    }
-    //this.nextYear = this.nextYear.bind(this);
-  }
-  nextYear = () =>  {
-        this.setState(state =>({
-            years: state.years + 1
-        }))
-  }
-  next = () => {
-        this.setState(state =>(
-            {
-          text: state.text +'*',
-          years: state.years - 1,
-      }
-        ))
-}
-    commitInputChanges = (e, color) => {
-        console.log(color);
-       
-        this.setState ({
-        position: e.target.value
-    })
-}
-
-    render() { 
-        
-            const {name, surname, link} = this.props;
-            const {position, years, text} = this.state;
-            return (
-                <div>
-                    <button onClick = {this.nextYear}>{text}</button>
-                    <button onClick={this.next}>{text}</button>
-                    <h1>My name is {name} , surname : {surname},
-                     my age {years}, This state position:{position} </h1>
-                    <a href={link}>My Profile</a>
-                    <from>
-                        <span>Enter employer</span>
-                        <input type="text"onChange= {(e) => this.commitInputChanges(e, 'some color')} />
-                    </from>
-                </div>
-                ) 
-        }
-}
-
-
+import React, { useState } from "react";
+import './App.scss';
 
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="App-header">
-        <WhoAmI name='Alex' surname='Borsuk' link='www.leningad.ru'/>
-        <WhoAmI name='Ivan' surname='ivanov' link='www.leningad.ru'/>
-        </h1>
-        
-    </div>
-  );
+
+const [posts, setPosts] = useState([]);
+const [body, setInput] = useState('');
+const [title, setTitle] = useState('');
+
+const  fetchPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10&_page=3');
+    const result = await response.json()
+        setPosts(result);
+}
+const deletePost = (id) => {
+    setPosts([...posts].filter(el => el.id !== id));
+}
+const addPost = (e) => {
+    const newPost = {
+        title, body, id: Date.now()
+    }
+    setPosts([...posts, newPost]);
 }
 
+return (
+      <div className="App-header">
+          <button onClick={fetchPosts}>Get posts</button>
+          <button onClick={() => addPost(posts)}>add post</button>
+          <input type="text" 
+                name="name" id="1" 
+                value={body}
+                onChange={e => {setInput(e.target.value)}}/>
+            <input type="text" 
+                name="title" id="1" 
+                value={title}
+                onChange={e => {setTitle(e.target.value)}}/>
+          {posts.map((item, index) =>        
+                   <div className="div" key={index}>
+                       <div>post#{index}</div><div>{item.title}</div>
+                       <div>{item.body}</div>
+                       <span>user ID{item.userId}</span>
+                       <span>Id {item.id}</span>
+                       <button onClick={() => deletePost(item.id)}>DELETE POST</button>
+                    </div>
+
+          )}
+      </div>
+    );
+}
 export default App;
-
-
